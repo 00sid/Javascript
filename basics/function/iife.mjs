674a638a40d7d2b54c31d1d864278fde18d50d36
Siddhartha Basnet <siddbasnet2002@gmail.com>
2024-10-08 09:14:46 +0545

    immediately invoked function expression

---
+++ basics/function/iife.mjs
@@ -0,0 +1,14 @@
+//immediately invoked function expression(IIFE)
+//syntax (function)()
+
+//named iife
+(function name() {
+  console.log("Siddhartha");
+})();
+
+//unnamed iife i.e arrow function
+
+((name) => {
+  console.log(name);
+})("Basnet");
+//passing variable at the end
